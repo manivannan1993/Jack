de4fdbd3cd191ed56565ac772f27e7ff32e4ce19
manivannan19 <mani11mer023@gmail.com>
2023-08-31 12:29:15 +0530

    new code

---
+++ src/Practics/Programmetwo.java
@@ -0,0 +1,17 @@
+package Practics;
+
+public class Programmetwo {
+	public static void main(String[] args) {
+//		int i=3;
+		
+		for(int i=1;i<=3;i++) {
+		
+			for(int j=1;j<=3;j++) {
+				if (i==2&&j==2) {
+					break;
+					}System.out.println(i+" "+j);
+			}
+		}
+	
+	}}
+
---
+++ src/Practics/programme.java
@@ -0,0 +1,35 @@
+
+ package Practics;
+ 
+public class programme{
+	
+	public static void main(String[] args) {
+//		int a=7;
+		for (int i = 0;i<=5;i++){
+			for (int j=5;j>=i;j--) {
+				System.out.print("*");
+				
+			}
+			System.out.println();}}}
+	
+	
+				
+			
+				
+				
+				
+			
+	
+ 
+	 
+	 
+	 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
